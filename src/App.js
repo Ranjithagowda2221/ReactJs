ecf829047ce13f1624e2e70f699637ec7e757401
Ranjitha <ranjithaislive@gmail.com>
2020-08-04 18:30:45 +0530

    Helloworld App using react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Components/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Ranjitha', age: '23', id: '1' },
+      { name: 'praveen', age: '25', id: '2' },
+      { name: 'bhaskar', age: '25', id: '3' }
+    ],
+    showPerson: true
+  }
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [{ name: newName, age: '23' }]
+    })
+  }
+  nameChngedHandlr = (event, id) => {
+
+    const personIndx = this.state.persons.findIndex(p => {
+      return p.id = id;
+    });
+
+    // person = [...this.state.persons[personIndx]];
+    debugger
+    // person.name = event.target.value;
+    // const persons = [...this.state.persons];
+    // persons[personIndex] = person;
+    // this.setState({
+    //   persons: persons
+    // })
+  }
+  toggleNameHandler = () => {
+    const doesShow = this.state.showPerson;
+    this.setState({
+      showPerson: !doesShow
+    })
+  }
+  deleteHnaldr = (event, personIndex) => {
+    const persons = this.state.persons.splice();
+    persons.splice(personIndex, 1);
+    this.setState({
+      persons: persons
+    });
+  }
+  render() {
+    let person = null;
+    if (this.state.showPerson) {
+      person = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              changed={(event) => this.nameChngedHandlr(event, person.id)}
+              key={person.id} name={person.name} age={person.age}
+              click={() => this.deleteHnaldr(index)} />
+          })}
+        </div>
+      );
+    }
+    return (
+      <div className="App">
+        <p>You got this. Make it happen!</p>
+        <button onClick={this.toggleNameHandler}>Swicth name</button>
+        {person}
+      </div>
+    );
+  }
+  // return React.createElement('div', { className: 'App' }, React.createElement('p', null, 'Yes! Possible'));
 }
 
 export default App;
---
+++ src/Components/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Person = (props) => {
+    return (
+        <div>
+            <p onClick={props.click}>Name: {props.name} Age : {props.age}</p>
+            <p> {props.children} </p>
+            <input type='text' onChange={props.changed} value={props.name} />
+        </div>
+    );
+}
+
+export default Person;
